eff28356b6470b070bc92f32b8e662cc5f4b392e
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-26 13:13:15 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this path based on your file structure
+    "./index.html", // Include your main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist", // Ensure the build output goes to the 'dist' directory
   },
