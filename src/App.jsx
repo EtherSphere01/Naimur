4775fccd88c1011dfa9d54337b3a92a96fa94ae3
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-01-10 20:43:38 +0600

    Experience added

--- src/App.jsx
+++ src/App.jsx
@@ -16,71 +16,74 @@ import { useEffect, useState, Suspense } from "react";
 import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 
 const Body = styled.div`
-  background-color: ${({ theme }) => theme.bg};
-  width: 100%;
-  overflow-x: hidden;
-  position: relative;
+    background-color: ${({ theme }) => theme.bg};
+    width: 100%;
+    overflow-x: hidden;
+    position: relative;
 `;
 
 const Wrapper = styled.div`
-  padding-bottom: 100px;
-  background: linear-gradient(
-      38.73deg,
-      rgba(204, 0, 187, 0.15) 0%,
-      rgba(201, 32, 184, 0) 50%
-    ),
-    linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
-    );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+    padding-bottom: 100px;
+    background: linear-gradient(
+            38.73deg,
+            rgba(204, 0, 187, 0.15) 0%,
+            rgba(201, 32, 184, 0) 50%
+        ),
+        linear-gradient(
+            141.27deg,
+            rgba(0, 70, 209, 0) 50%,
+            rgba(0, 70, 209, 0.15) 100%
+        );
+    width: 100%;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
 `;
 
 function App() {
-  const [openModal, setOpenModal] = useState({ state: false, project: null });
-  const [loading, setLoading] = useState(true);
+    const [openModal, setOpenModal] = useState({ state: false, project: null });
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2000);
-    return () => clearTimeout(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <Body>
-          <BrowserRouter>
-            <Navbar />
-            <StarCanvas />
-            <AnimatePresence>
-              <div>
-                <Hero />
-                <Wrapper>
-                  <Skills />
-                  {/* <Experience /> */}
-                </Wrapper>
-                <Projects openModal={openModal} setOpenModal={setOpenModal} />
-                <Wrapper>
-                  <Education />
-                  <Contact />
-                </Wrapper>
-                <Footer />
-                {openModal.state && (
-                  <ProjectDetails
-                    openModal={openModal}
-                    setOpenModal={setOpenModal}
-                  />
-                )}
-              </div>
-            </AnimatePresence>
-          </BrowserRouter>
-        </Body>
-      )}
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            {loading ? (
+                <LoadingScreen />
+            ) : (
+                <Body>
+                    <BrowserRouter>
+                        <Navbar />
+                        <StarCanvas />
+                        <AnimatePresence>
+                            <div>
+                                <Hero />
+                                <Wrapper>
+                                    <Skills />
+                                    <Experience />
+                                </Wrapper>
+                                <Projects
+                                    openModal={openModal}
+                                    setOpenModal={setOpenModal}
+                                />
+                                <Wrapper>
+                                    <Education />
+                                    <Contact />
+                                </Wrapper>
+                                <Footer />
+                                {openModal.state && (
+                                    <ProjectDetails
+                                        openModal={openModal}
+                                        setOpenModal={setOpenModal}
+                                    />
+                                )}
+                            </div>
+                        </AnimatePresence>
+                    </BrowserRouter>
+                </Body>
+            )}
+        </ThemeProvider>
+    );
 }
 export default App;
